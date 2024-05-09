0bce7982e8429cd5b5b66956d41d2631ca8e62a2
luisfurquim <luisfurquim@gmail.com>
2024-05-08 22:56:24 -0300

    @{debug}

--- oauth2mixed/Oauth2T.ExtAuthorize.go
+++ oauth2mixed/Oauth2T.ExtAuthorize.go
@@ -276,7 +276,8 @@ main:
 
 //			bearer.AccessToken = strings.Join(strings.Split(bearer.AccessToken,".")[:3],".")
 
-			body = `token=` + url.PathEscape(tok.AccessToken[7:])
+			body = `token=` + tok.AccessToken[7:]
+//			body = `token=` + url.PathEscape(tok.AccessToken[7:])
 //			body = `token=` + url.PathEscape(strings.Join(strings.Split(tok.AccessToken[7:],".")[:3],"."))
 //			strings.Join(strings.Split(tok.AccessToken[7:],".")[:3],".")
 
